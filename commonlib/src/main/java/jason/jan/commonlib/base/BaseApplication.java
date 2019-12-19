97e9531862feb7d27981efc2f6894b0e58145aa5
wangja <wangja@coocent.net>
2019-12-19 09:10:05 +0800

    modify gradle.

--- commonlib/src/main/java/jason/jan/commonlib/base/BaseApplication.java
+++ commonlib/src/main/java/jason/jan/commonlib/base/BaseApplication.java
@@ -7,30 +7,20 @@ import com.alibaba.android.arouter.launcher.ARouter;
 
 /**
  * Description: Application基类
+ * 加了一个Aouter而已
  * *
  * Creator: Wang
  * Date: 2019/12/18 23:46
  */
 public class BaseApplication extends Application {
 
-    private static  BaseApplication instance;
+    private static BaseApplication instance;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance=this;
         initRouter();
-    }
-
-    public static BaseApplication getInstance(){
-        return instance;
-    }
-    private void initRouter(){
-        if (BuildConfig.DEBUG) {
-            ARouter.openDebug();
-            ARouter.openLog();
-        }
-        ARouter.init(this);
     }
 
     @Override
@@ -46,6 +36,18 @@ public class BaseApplication extends Application {
     @Override
     public void onLowMemory() {
         super.onLowMemory();
+    }
+
+    public static BaseApplication getInstance(){
+        return instance;
+    }
+
+    private void initRouter(){
+        if (BuildConfig.DEBUG) {
+            ARouter.openDebug();
+            ARouter.openLog();
+        }
+        ARouter.init(this);
     }
 
 }
---
+++ main/src/main/java/jason/jan/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package jason.jan.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jason.jan.commonlib.IAppComponent
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
