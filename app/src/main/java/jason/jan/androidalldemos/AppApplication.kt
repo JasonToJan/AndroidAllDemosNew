3fa72862d129a6dec7a5905081a6c0f339fbef48
JasonJan <jason1211241203@gmail.com>
2019-12-19 23:38:20 +0800

    add main add commonlib

---
+++ app/src/main/java/jason/jan/androidalldemos/AppApplication.kt
@@ -0,0 +1,14 @@
+package jason.jan.androidalldemos
+
+import jason.jan.commonlib.base.BaseApplication
+
+/**
+ * App 层的Application
+ */
+class AppApplication : BaseApplication() {
+
+    override fun onCreate() {
+      super.onCreate()
+    }
+
+}
--- main/src/main/java/jason/jan/main/MainActivity.kt
+++ main/src/main/java/jason/jan/main/MainActivity.kt
@@ -2,9 +2,11 @@ package jason.jan.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import jason.jan.commonlib.IAppComponent
+import jason.jan.commonlib.base.BaseApplication
 
 class MainActivity : AppCompatActivity() {
+
+    var app : BaseApplication ? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
