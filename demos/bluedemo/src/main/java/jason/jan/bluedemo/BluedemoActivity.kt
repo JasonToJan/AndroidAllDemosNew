7d72bdc46ca8273bbc020bd636bbd83f9b33ee19
wangja <wangja@coocent.net>
2019-12-20 20:18:16 +0800

    可以运行了，现在，单独或者继承 均可。

---
+++ demos/bluedemo/src/main/java/jason/jan/bluedemo/BluedemoActivity.kt
@@ -0,0 +1,13 @@
+package jason.jan.bluedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jason.jan.main.R
+
+class BluedemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluedemo)
+    }
+}
